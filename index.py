bc91addb5de690d4883dbd5f6c7ffebf1452d020
Jasper Moelker <j.b.moelker@activeids.nl>
2016-09-12 12:11:07 +0200

    feat(jinja2): write errors to *.error.log files

--- index.py
+++ index.py
@@ -5,6 +5,7 @@ from jinja2 import Environment, FileSystemLoader
 
 inputDir = 'tests/'
 outputDir = 'output/jinja2/'
+errorExt = '.error.log'
 
 env = Environment(loader=FileSystemLoader(inputDir))
 
@@ -13,22 +14,44 @@ def list_html_files(dir):
     paths = [file[len(dir):] for file in files]
     return paths
 
-for filename in list_html_files(inputDir):
+
+def get_data(filename):
     data = {}
 
-    (basename, ext) = os.path.splitext(inputDir + filename)
+    (basename, ext) = os.path.splitext(filename)
     dataFilename = basename + '.json'
     if (os.path.isfile(dataFilename)):
         with open(dataFilename) as data_file:
             data = json.load(data_file)
 
-    template = env.get_template(filename)
-    output = template.render(**data)
+    return data
 
-    dir = os.path.dirname(outputDir + filename)
+
+def make_dir(filename):
+    dir = os.path.dirname(filename)
     if not os.path.isdir(dir):
         os.makedirs(dir)
 
-    outputFile = open(outputDir + filename, 'w')
-    outputFile.write(output)
+
+def save_file(filename, content):
+    make_dir(filename)
+    outputFile = open(filename, 'w')
+    outputFile.write(content)
     outputFile.close()
+
+
+def write_error_log(filename, error):
+    (basename, ext) = os.path.splitext(filename)
+    errorFilename = basename + errorExt
+    save_file(errorFilename, error)
+
+
+for filename in list_html_files(inputDir):
+    data = get_data(inputDir + filename)
+
+    try:
+        template = env.get_template(filename)
+        output = template.render(**data)
+        save_file(outputDir + filename, output)
+    except Exception as e:
+        write_error_log(outputDir + filename, str(e))
