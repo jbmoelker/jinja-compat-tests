33a376a3fb1997528a06e561e2dfe8ef0f83944c
Jasper Moelker <j.b.moelker@activeids.nl>
2016-09-12 14:46:24 +0200

    feat(output): write tests/ file contents to output/tests.json
    can be used for ui

--- index.js
+++ index.js
@@ -1,5 +1,3 @@
-const glob = require('glob');
-const merge = require('lodash').merge;
 const saveJson = require('./lib/save-json');
 const tree = require('./lib/file-contents-tree');
 const versions = require('./lib/versions');
@@ -9,23 +7,3 @@ const outputDir = 'output/';
 
 saveJson(outputDir + 'tests.json', tree(inputDir, '.html'));
 saveJson(outputDir + 'versions.json', versions);
-
-glob('*/', { cwd: outputDir }, (err, dirnames) => {
-    if (err) {
-        return console.error(err);
-    }
-
-    const obj = dirnames
-        .map(dirname => dirname.substring(0, dirname.length -1))
-        .map(engineName => {
-            const results = tree(`${outputDir}${engineName}/`, '.html');
-            const logs = tree(`${outputDir}${engineName}/`, '.error.log');
-            const contents = merge({}, results, logs);
-            return { engineName, contents };
-        }).reduce((output, item) => {
-            output[item.engineName] = item.contents;
-            return output;
-        }, {});
-
-    saveJson(outputDir + 'output.json', obj);
-});
