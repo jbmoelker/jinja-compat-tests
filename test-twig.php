29e2f99fbc6774a2096dc87b3feee6cfe5f3a42c
Jasper Moelker <j.b.moelker@activeids.nl>
2016-09-18 17:44:41 +0200

    feat(test-twig): replace warnings by formatted errors

--- test-twig.php
+++ test-twig.php
@@ -13,6 +13,29 @@ $errorExt = '.error.log';
 $finder = new Finder(); // http://symfony.com/doc/current/components/finder.html
 $fs = new Filesystem(); // http://symfony.com/doc/current/components/filesystem.html
 
+function startsWith($haystack, $needle) {
+     return (substr($haystack, 0, strlen($needle)) === $needle);
+}
+
+class Twig_Warning extends Exception
+{
+    protected $rawMessage;
+
+    public function __construct($message) {
+        $this->setRawMessage($message);
+    }
+
+    public function getRawMessage() {
+        return $this->rawMessage;
+    }
+
+    protected function setRawMessage($message) {
+        $rootDir = preg_quote(dirname(__FILE__), '/');
+        $rawMessage = preg_replace('/ in ' . $rootDir . '.* on line \d+/', '', $message);
+        $this->rawMessage = $rawMessage;
+    }
+}
+
 $loader = new Twig_Loader_Filesystem($inputDir);
 $twig = new Twig_Environment($loader, array());
 
@@ -26,6 +49,9 @@ foreach ($files as $file) {
 
     try {
         $output = $twig->render($filename, $data);
+        if (startsWith(trim($output), 'Warning: ')) {
+            throw new Twig_Warning($output);
+        }
         $fs->dumpFile($outputDir . $filename, $output);
     } catch (Exception $e) {
         $errorFilename = str_replace('.html', $errorExt, ($outputDir . $filename));
